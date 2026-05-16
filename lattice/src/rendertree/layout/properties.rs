9a20332852e853aef391502a8ac0d731d26ca3b3
Antoine van Wel <antoinevanwel@gmail.com>
2026-05-16 10:32:15 +0200

    SImplify property matching

--- lattice/src/rendertree/layout/properties.rs
+++ lattice/src/rendertree/layout/properties.rs
@@ -5,30 +5,13 @@ use super::util::parse_dimension;
 
 pub fn set_property(style: &mut Style, property: &str, value: Value<'_>) -> Option<bool> {
   match property {
-    "width" => {
-      style.size.width = parse_dimension(value);
-      Some(true)
-    }
-    "height" => {
-      style.size.height = parse_dimension(value);
-      Some(true)
-    }
-    "minWidth" => {
-      style.min_size.width = parse_dimension(value);
-      Some(true)
-    }
-    "minHeight" => {
-      style.min_size.height = parse_dimension(value);
-      Some(true)
-    }
-    "maxWidth" => {
-      style.max_size.width = parse_dimension(value);
-      Some(true)
-    }
-    "maxHeight" => {
-      style.max_size.height = parse_dimension(value);
-      Some(true)
-    }
-    _ => None,
+    "width"     => style.size.width      = parse_dimension(value),
+    "height"    => style.size.height     = parse_dimension(value),
+    "minWidth"  => style.min_size.width  = parse_dimension(value),
+    "minHeight" => style.min_size.height = parse_dimension(value),
+    "maxWidth"  => style.max_size.width  = parse_dimension(value),
+    "maxHeight" => style.max_size.height = parse_dimension(value),
+    _ => return None,
   }
+  Some(true)
 }
