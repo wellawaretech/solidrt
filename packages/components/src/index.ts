b3b075aa98483ac3df0c239ed7ab13158b949799
Antoine van Wel <antoinevanwel@gmail.com>
2026-05-24 09:18:30 +0200

    File rename

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,3 @@
-export { SafeArea } from "./SafeArea"
-export { TextInput, type TextInputProps } from "./TextInput"
+export { SafeArea } from "./safe-area"
+export { TextInput, type TextInputProps } from "./text-input"
 export { theme, setTheme, type Theme } from "./theme"
