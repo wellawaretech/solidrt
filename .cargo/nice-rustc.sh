0867b4f6d2dcaf060749fceb8a30bf90bf18cb89
Antoine van Wel <antoinevanwel@gmail.com>
2026-06-29 11:01:58 +0200

    Revert niceness, should have been a local change

--- .cargo/nice-rustc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Run rustc (and the linker it spawns) at low CPU + idle I/O priority so a
-# full-throttle build still yields to the desktop. Wired in via
-# build.rustc-wrapper in .cargo/config.toml; applies to any `cargo`/`make`
-# invocation in this workspace regardless of cwd. cargo passes the real rustc
-# path as the first argument.
-exec nice -n 19 ionice -c 3 "$@"
